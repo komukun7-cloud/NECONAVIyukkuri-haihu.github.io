6674db523528e7ac9a77d06fabf773b0fa2c4daf
miikun95 <miikun95@miikun95.net>
2026-03-25 15:47:43 +0900

    Update pass.js

--- pass.js
+++ pass.js
@@ -1,5 +1,5 @@
 "use strict";
-var pwInput = document.getElementById("password");
+var pwInput = document.getElementById("pw");
 document.getElementById("go").addEventListener("click", function() {
   if (pwInput.value) {
     location.href = "/secret/" + pwInput.value + "/";
