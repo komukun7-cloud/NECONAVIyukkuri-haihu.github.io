c0743888a493bc35a06e21df819b135f59ccb814
miikun95 <miikun95@miikun95.net>
2026-03-25 15:39:05 +0900

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,9 @@
+"use strict";
+var pwInput = document.getElementById("password");
+document.getElementById("go").addEventListener("click", function
+  if (pwInput.value) {
+    location.href = "/secret/" + pwInput.value + "/";
+  } else {
+    alert("パスワードを入れてね！");
+}
+});
